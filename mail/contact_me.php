3778a1037d22af875ea21d90a2e4d8eeec7c76de
Erin Gallagher <erin.gallagher@td.com>
2017-08-07 10:33:48 -0400

    update contact from

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,12 +20,13 @@ $to = 'erin.catherine.gallagher@gmail.com'; // Add your email address inbetween 
 $email_subject = "Impact Construction Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@impactconstruction.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 
 //confirmation email
 $to_confirm = $email_address;
 $email_subject_confrim = "Impact Construction: We received your email!";
-$email_body_confirm = "Thank you for reaching out to Impact Construction. We have received your email and will get back to you soon. \n\n Thank You, \nNeil Gallagher \n(416)-995-6933";
+$email_body_confirm = "Thank you for reaching out to Impact Construction. We have received your email and will get back to you soon. \n\nThank You, \n\nNeil Gallagher \n(416)-995-6933";
 $headers = "From: noreply@impactconstruction.com\n";
 mail($to_confirm,$email_subject_confrim,$email_body_confirm,$headers);
 return true;         
